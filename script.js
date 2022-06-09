c2785374c5bfffbeba7dce8718ab18a833e455bf
root <root@DESKTOP-6B3FQL8.localdomain>
2022-06-09 15:55:28 -0500

    add function

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-thinkNum = 13;
-let wrongAns = [];
+"use strict"
 
-var loggedin = false;
-var attempt = 4;
-while(!loggedin && attempt > 0){
-var userAns = prompt("Think of a number between 1 and 3").toLowerCase();
- if(userAns=="2" || userAns== "two"){
-   loggedin = true;
- }
- else
- {
-attempt --;
-alert("You have left "+attempt+" attempt;");
-if( attempt == 0)
- {
-  wrongAns.push(1);
-   alert('The answer is 2');
- }
-}
-}
-//
 var name = window.prompt("Hey, you have a name?")
 alert("Hey " + name + ", nice to chat with you!")
-//
 
+function game(){
 userAns = prompt("I'd like to ask you some questions, is that ok?");
 alert("Ok, let's move along please!");
-//
+
 var loggedin = false;
 var attempt = 6;
 while(!loggedin && attempt > 0){
@@ -122,31 +102,15 @@ if( attempt == 0)
  }
 }
 }
-// //
-let thisArray = [4,6,8,10];
-
-var loggedin = false;
-var attempt = 6;
-while(!loggedin && attempt > 0){
-var userAns = prompt("How many fingers did I have up? Four even numbers between 2 and 12.").toLowerCase();
- if(userAns== thisArray){
-   loggedin = true;
- }
- else
- {
-attempt --;
-alert("You have left "+attempt+" attempt;");
-if( attempt == 0)
- {
-  wrongAns.push(1);
-   alert('Well...I have no response');
- }
+for(let attempt = 6; attempt > 0;attempt--){
+let thisArray = /['4','6','8','10']/
+let userAns = prompt("How many fingers did I have up? Four even numbers between 3 and 12.");//.toLowerCase();
+ if(userAns.match(thisArray)){
+   alert("correct")
+   break;
+ }else{
+   alert("You have left "+attempt+" attempt;");
 }
 }
-
-var questions = 7;
-
-alert(`You got an answer wrong ${wrongAns.length} times out of ${questions}!`);
-
-//
-
+}
+game()
